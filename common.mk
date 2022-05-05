899b6f1cef5877b5ff4252b589588a8e8eeda901
Dylan Neve <dylanneve1@gmail.com>
2022-05-05 12:30:12 +0100

    exynos9810-common: sensors: Adapt for MultiHAL

--- common.mk
+++ common.mk
@@ -266,9 +266,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.schedulerservice@1.0.vendor:64 \
-    android.hardware.sensors@1.0-impl.samsung:64 \
-    android.hardware.sensors@1.0-service \
+    android.hardware.contexthub@1.0.vendor:64 \
+    android.hardware.sensors@2.1-service.samsung-multihal \
     libsensorndkbridge
 
 # Shims

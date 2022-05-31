0f444ffb9a1d827a8929184034257bbc08575a13
Mathieu Chartier <mathieuc@google.com>
2022-06-01 00:41:45 +0100

    exynos9810-common: Compile system services using speed-profile

--- common.mk
+++ common.mk
@@ -295,6 +295,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/samsung/aidl/power-libperfmgr
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \

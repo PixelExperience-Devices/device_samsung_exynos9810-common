4b47c09dabc7d4b4f73600f1508804c183a1aa07
Dylan Neve <dylanneve1@gmail.com>
2022-06-17 18:25:45 +0100

    exynos9810-common: telephony: Recognize more APNs out of the box

--- common.mk
+++ common.mk
@@ -24,6 +24,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+# APNs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
